0806a8baf4201640579553b97872dff8d8138696
Lilian Zhao <milanzq309@gmail.com>
2022-09-17 18:44:25 -0400

    started sign in page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import './App.css';
-import { createNote, deleteNote} from './graphql/mutations'
-import { listNotes } from './graphql/queries'
-import { useCallback, useEffect, useState } from 'react';
-import { API } from 'aws-amplify';
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 import NavBar from './components/NavBar'
 import DisplayData from './components/DisplayData.js'
-
-//import Typing from './node_modules/typing-animate/typing.js'
+import SignUp from './components/SignUp'
 
 function App({ signOut }) {
-
-    /*const el = document.getElementById('el');
-    const string = 'Your text...'
-
-    new Typing({
-        element: el,
-        text: string,
-        hideClass: hideElement,
-        typingDelay: 70
-    }).run();*/
-
     return (
         <div>
             <NavBar/>
             <div className='frame landing-page'>
-                <div className='landing-title'>RUA_live?</div>
+                <div className='landing-title' id='landing-title'>RUA_live?</div>
                 <button className='sos-btn'>SOS</button>
             </div>
             <DisplayData/>
+            <SignUp/>
         </div>
     );
 }
 
-export default /*withAuthenticator(*/App/*)*/;
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import '../SignUp.css';
+
+function SignUp() {
+    return (
+        <div className='transparent-container'>
+            <div>dsf</div>
+        </div>
+    );
+}
+
+export default SignUp;
