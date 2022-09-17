2d323274eff9ae799701c2ae817ea135c711b397
Lilian Zhao <milanzq309@gmail.com>
2022-09-17 14:07:43 -0400

    fixed webvitals error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 import "@aws-amplify/ui-react/styles.css"; // Ensure React UI libraries are styled correctly
 import { Amplify } from 'aws-amplify'
